54c7a8083e08ae8d175c7e694fa7e2a4a9c7c82a
AdamAkkari <theadamsmccall@gmail.com>
2023-01-08 14:37:21 +0100

    Modified camera

--- Ld- 52 -harvest_game/Elements/Player/GameCamera.gd
+++ Ld- 52 -harvest_game/Elements/Player/GameCamera.gd
@@ -5,6 +5,6 @@ export var player_path : NodePath
 onready var player := get_node(player_path)
 
 func _process(delta):
-	translation.z = player.translation.z - 5
+	translation.z = player.translation.z - 3
 	translation.x = player.translation.x / 1.5
-	translation.y = 3
+	translation.y = 2
